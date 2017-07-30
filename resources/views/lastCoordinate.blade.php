90da42e5f20e426e4466fb22475469d1aad6549f
Fabián Quintero <fabian.quintero@triolabs.mx>
2017-07-30 15:29:58 -0500

    Show last coordinate map

--- resources/views/lastCoordinate.blade.php
+++ resources/views/lastCoordinate.blade.php
@@ -4,6 +4,8 @@
     Ingresa el nombre usuario:<input id="input_user" name="user" type="text"><br>
     <button id="send">Enviar</button><br><br>
     <div id="last-coordinate"></div>
+    <div style="display: none" id="map"></div>
+
 @endsection
 @section('head_script')
     <script>
@@ -25,12 +27,50 @@
                             str = data.lat+','+data.lon;
                             //TODO: instead showing the coordinate, paint on google maps
                             $('#last-coordinate').text(str);
+                            $('#map').show();
+                            initMap(data.lat, data.lon)
                         }
 
 
                     }
                 });
             });
+
+
+            function paintOnGoogleMaps(lat, lon){
+                //key = 'AIzaSyBw1NAYieikzKLR8p_BI8eBShWDTGEh8Mg'
+            }
+
+
         })
     </script>
+    <script>
+        function initMap(lat = 0, lon=0) {
+            lat = parseFloat(lat)
+            lon = parseFloat(lon)
+
+            var uluru = {lat: lat, lng: lon};
+            var map = new google.maps.Map(document.getElementById('map'), {
+                zoom: 8,
+                center: uluru
+            });
+            var marker = new google.maps.Marker({
+                position: uluru,
+                map: map
+            });
+        }
+    </script>
+    <script async defer
+            src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBw1NAYieikzKLR8p_BI8eBShWDTGEh8Mg&callback=initMap">
+    </script>
+@endsection
+
+@section('css')
+<style>
+    #map {
+        width: 100%;
+        height: 400px;
+        background-color: grey;
+    }
+</style>
 @endsection
